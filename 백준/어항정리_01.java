9abebfa7e4ab20693a12a48f56ebb1f1cda9bc7c
shpark0308 <60208434+shpark0308@users.noreply.github.com>
2022-01-13 21:53:51 +0900

    2021.01.03 D-46 need more effort / updated

---
+++ 백준/어항정리_01.java
@@ -0,0 +1,12 @@
+package 백준;
+
+public class 어항정리_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder s = new StringBuilder("SSSSS");
+		System.out.println(s.indexOf("1"));
+
+	}
+
+}
